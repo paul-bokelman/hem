a4cd7c87e44589913499453cd8b09aae02f82ddb
Paul Bokelman <paul.bokelman1@gmail.com>
2025-04-26 15:57:45 -0600

    gunicorn for flask

--- server/app.py
+++ server/app.py
@@ -12,9 +12,10 @@ preflight()
 app = Flask(__name__)
 CORS(app)
 
-# create the database tables if they don't exist
-with db:
-    db.create_tables([User, Action, Macro, MacroAction, UserAction])
+def init_db():
+    """Initialize the database and create tables if they don't exist."""
+    with db:
+        db.create_tables([User, Action, Macro, MacroAction, UserAction])
 
 # register route blueprints
 app.register_blueprint(users_bp)
@@ -23,4 +24,5 @@ app.register_blueprint(actions_bp)
 app.register_blueprint(pipeline_bp)
 
 if __name__ == '__main__':
+    init_db()
     app.run(port=8000, debug=True)
